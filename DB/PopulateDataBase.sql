36d5b0f9bb8653ae34399fd69d3333b4f624d8e6
SoBeRBot94 <sudarsan.bhargavan94@gmail.com>
2018-05-16 00:02:37 +0200

    Initial Script To Populate The Required DataBase Tables

---
+++ DB/PopulateDataBase.sql
@@ -0,0 +1,15 @@
+/* File To Populate DataBase Tables */
+
+/* Change To automated_warehouse_management */
+
+USE automated_warehouse_management;
+
+INSERT INTO storage_unit
+	(storage_unit_name, no_of_items, max_capacity) 
+VALUES
+	('A', 0, 1),
+	('B', 0, 1),
+	('c', 0, 1),
+	('D', 0, 1),
+	('E', 0, 1),
+	('F', 0, 1);
