bdac391353149b73d6fa9ae7e7763254402eb87c
Wang Zijian <791264377@qq.com>
2018-05-22 15:34:42 +0300

    Update server2.py

--- asn2/server2.py
+++ asn2/server2.py
@@ -10,16 +10,13 @@ buf = 1024
 addr = (host, port)
 UDPSock = socket(AF_INET, SOCK_DGRAM)
 UDPSock.bind(addr)
-print ('Adding button')
-btn = Button()
+
 def forward(lm, rm):
     lm.polarity = "normal"
     rm.polarity = "normal"
     lm.run_forever(speed_sp=100)
     rm.run_forever(speed_sp=100)
-    #sleep(time)
-    #lm.stop()
-    #rm.stop()
+    
 def turnright(lm, rm):
     sleep(0.5)
     gy.mode='GYRO-RATE'
@@ -27,7 +24,7 @@ def turnright(lm, rm):
     lm.polarity = "normal"
     rm.polarity = "normal"
     lm.run_forever(speed_sp=100)
-    rm.run_forever(speed_sp=0)
+    rm.stop()
     #while gy.value()< 90:
         #sleep(0.02)
     #lm.stop()
@@ -41,13 +38,18 @@ assert gy.connected
 assert rm.connected
 assert lm.connected
 assert lf.connected
-while not btn.any():
-    (data, addr) = UDPSock.recvfrom(buf)
-    data=str(data, encoding="utf-8")
-    print "Received message: " + data
-    if data=="go forward":
-        forward(lm, rm)
-    elif data=="turn right"
-        turnright(lm, rm)
-UDPSock.close()
-os._exit(0)
+try:
+#while not btn.any():
+    while 1:
+        (data, addr) = UDPSock.recvfrom(buf)
+        data=str(data, encoding="utf-8")
+        print "Received message: " + data
+        if data=="go forward":
+            forward(lm, rm)
+        elif data=="turn right"
+            turnright(lm, rm)
+except KeyboardInterrupt:
+    lm.stop()
+    rm.stop()
+    UDPSock.close()
+    os._exit(0)
